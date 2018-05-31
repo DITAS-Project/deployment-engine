22b2aa29eb36144792ef3d38d95d96e8b881fd14
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2018-05-31 16:40:42 +0200

    Python stupid mistake

--- src/kubernetes/mysql.py
+++ src/kubernetes/mysql.py
@@ -2,6 +2,6 @@
 def update_ip_status(dict_ip, dict_status, cur, db):
 
     for key in set(dict_ip.keys()) & set(dict_status.keys()):
-        cur.execute("""UPDATE nodes SET nodesBlueprint.public_ip = %s, nodesBlueprint.status = %s WHERE nodesBlueprint.id = %s""",
+        cur.execute("""UPDATE nodesBlueprint SET nodesBlueprint.public_ip = %s, nodesBlueprint.status = %s WHERE nodesBlueprint.id = %s""",
                     (dict_ip[key], dict_status[key], key))
         db.commit()
