6b8a9607342ecc6fe966a1a6e2c763c8bfa18ea0
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2018-06-14 14:31:04 +0200

    Typo

--- src/kubernetes/scripts/SLAdeploy.sh
+++ src/kubernetes/scripts/SLAdeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -o pod_deploy.yaml https://raw.githubusercontent.com/DITAS-Project/SLALite/master/pod_deploy.yaml
-sed -i "s/name: slalite/$name: slalite$(date +%Y%m%d%H%M%S)/g" pod_deploy.yaml
+sed -i "s/name: slalite/name: slalite$(date +%Y%m%d%H%M%S)/g" pod_deploy.yaml
 export KUBECONFIG=$HOME/admin.conf
 kubectl create -f pod_deploy.yaml
 
