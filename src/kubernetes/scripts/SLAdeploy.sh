b96565d87d5ccaf2a259ac6ed67dc9786f3cb963
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2018-06-14 14:13:02 +0200

    Adding date to k8 component's name

--- src/kubernetes/scripts/SLAdeploy.sh
+++ src/kubernetes/scripts/SLAdeploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+curl -o pod_deploy.yaml https://raw.githubusercontent.com/DITAS-Project/SLALite/master/pod_deploy.yaml
+sed -i "s/name: slalite/$name: slalite$(date +%Y%m%d%H%M%S)/g" pod_deploy.yaml
 export KUBECONFIG=$HOME/admin.conf
-kubectl create -f https://raw.githubusercontent.com/DITAS-Project/SLALite/master/pod_deploy.yaml
+kubectl create -f pod_deploy.yaml
 
