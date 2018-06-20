7a70658f75f39b0fc43bd0462fafd82050f2847d
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2018-06-20 14:58:51 +0200

    Full deployment with separate volumes - 1st attempt

--- src/kubernetes/scripts/ds4mdeploy.sh
+++ src/kubernetes/scripts/ds4mdeploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 curl -o ds4m.yaml https://raw.githubusercontent.com/DITAS-Project/deployment-engine/master/kubernetes-components/ds4m.yaml
-sed -i "s/name: ds4m/name: ds4m$(date +%Y%m%d%H%M%S)/g" ds4m.yaml
+sed -i "s/name: ds4m/name: ds4m$1/g" ds4m.yaml
+sed -i "s/path: \/home\/cloudsigma\/blueprint/path: \/home\/cloudsigma\/blueprint$1/g" ds4m.yaml
+
 export KUBECONFIG=$HOME/admin.conf
 kubectl create -f ds4m.yaml
 
