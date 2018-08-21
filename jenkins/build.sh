76429a126aa16487a5d6c15b0ce9205a3f5bc126
Jose Antonio Sanchez <jose.sanchezm@atos.net>
2018-08-21 16:33:01 +0200

    Put timestamps

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env sh
-echo "Setting GOPATH"
+
+echo_time() {
+    date +"%R $*"
+}
+
+echo_time "Setting GOPATH"
 WORKDIR=$GOPATH/src/deployment-engine
 mkdir $WORKDIR
-echo "Copying sources"
+echo_time "Copying sources"
 cp -a . $WORKDIR
 cd $WORKDIR
-echo "Ensure dependencies"
+echo_time "Ensure dependencies"
 dep ensure
 cd src
-echo "Building"
+echo_time "Building"
 CGO_ENABLED=0 GOOS=linux go build -a -o deployment-engine
-echo "Testing"
+echo_time "Testing"
 go test ./...
-echo "Copying result to workspace"
+echo_time "Copying result to workspace"
 cp deployment-engine $1/src
-echo "Done"
+echo_time "Done"
