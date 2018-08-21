c4255db4de1b5804dd4c6d4a435a139975a3fd10
Jose Antonio Sanchez <jose.sanchezm@atos.net>
2018-08-21 16:24:05 +0200

    Log build script to see why it takes so long

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
+echo "Setting GOPATH"
 WORKDIR=$GOPATH/src/deployment-engine
 mkdir $WORKDIR
+echo "Copying sources"
 cp -a . $WORKDIR
 cd $WORKDIR
+echo "Ensure dependencies"
 dep ensure
 cd src
+echo "Building"
 CGO_ENABLED=0 GOOS=linux go build -a -o deployment-engine
+echo "Testing"
 go test ./...
+echo "Copying result to workspace"
 cp deployment-engine $1/src
+echo "Done"
